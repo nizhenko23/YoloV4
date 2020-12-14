92dd53bdc6e7b18feb2e5710e95a2689beb959bd
nizhenko23 <45242490+nizhenko23@users.noreply.github.com>
2020-12-14 16:16:17 +0300

    Update detection_custom.py

--- TensorFlow implementation/detection_custom.py
+++ TensorFlow implementation/detection_custom.py
@@ -10,8 +10,8 @@ image_path   = "./IMAGES/"
 video_path   = "./IMAGES/"
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/................", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_image(yolo, image_path, "./IMAGES/................", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
