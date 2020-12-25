3714f3e2d0a5b33b95f055e6501197d8f8b47108
nizhenko23 <45242490+nizhenko23@users.noreply.github.com>
2020-12-25 13:57:29 +0300

    Update blender_load.py

--- TensorFlow implementation/fly/blender_load.py
+++ TensorFlow implementation/fly/blender_load.py
@@ -58,7 +58,7 @@ co = bpy.context.scene.cursor_location
 co_2d = bpy_extras.object_utils.world_to_camera_view(scene, obj, co)
 print("2D Coords:", co_2d)
 
-
+# If you want pixel coords
 render_scale = scene.render.resolution_percentage / 100
 render_size = (
     int(scene.render.resolution_x * render_scale),
